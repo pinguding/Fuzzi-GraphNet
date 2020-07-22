fa1f641bca31b32c297fabd540a0a39dab5a43f3
pinguding <41717656+pinguding@users.noreply.github.com>
2020-07-22 15:32:58 +0900

    Start Project

---
+++ Fuzzy-GraphNet.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib as plot
+import fcmeans as FCM
+
+#DI_Train_File = open('./DI_Train_Data.csv','r')
+#STAI_T_Train_File = open('./STAI_T_Train_Data.csv','r')
+#STAI_S_Train_File = open('./STAI_S_Train_Data.csv','r')
+
+DI_Train_Data = pd.read_csv("./DI_Train_Data.csv")
+
